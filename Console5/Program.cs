9c8a855612f27e4ac3386ebcac752238bb425846
Alberto Viezzi - External <alberto.viezzi.external@ynap.com>
2022-05-12 10:25:44 +0200

    Add Console

---
+++ Console5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Synergie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# 7.0
+            int SumOne(int a)
+            {
+                return a + 1;
+            }
+            
+            // C# 8.0
+            static int SumOneS(int a)
+            {
+                return a + 1;
+            }
+
+
+            Console.WriteLine("Hello World!");
+            var r = SumOne(1);
+            int? a = null;
+            Test(a);
+            
+            
+            Console.ReadKey();
+        }
+        
+        public static void Test(int? a) => Console.WriteLine(a);
+    }
+}
